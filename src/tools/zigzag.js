ee703f7ce666842d7b65d68115a5188928dc9067
tangs <tangsong1988kaka@gmail.com>
2019-12-17 11:28:58 +0800

    refactor.

--- src/tools/zigzag.js
+++ src/tools/zigzag.js
@@ -1,42 +1,22 @@
-function encode(num, bits) {
+// @flow
+
+function encode(num: number, bits: number) {
   return (num << 1) ^ (num >>> (bits * 8 - 1))
 }
 
-function decode(num, bits) {
+function decode(num: number, bits: number) {
   return (num >> 1) ^ ((num & 1) == 1 ? 
     (0xffffffffffffffff >> (64 - bits * 8)) : 0)
 }
 
-function encode32(num) {
-  return (num << 1) ^ (num >> 31)
-}
-
-function decode32(num) {
-  return (num >>> 1) ^ -(num & 1)
-}
-
-function encode64(num) {
+function encode64(num: any) {
+  // $FlowFixMe
   return (num << 1n) ^ (num >> 63n)
 }
 
-function encode64low(low, high) {
-  return (low << 1) ^ (high >> 31)
-}
-
-function encode64high(low, high) {
- return (low >>> 31) ^ (high << 1) ^ (high >> 31)
-}
-
-function decode64(num) {
+function decode64(num: any) {
+  // $FlowFixMe
   return (num >> 1n) ^ ((num & 1n) == 1n ? 0xffffffffffffffffn : 0n)
-}
-
-function decode64low(low, high) {
-  return (high << 31) ^ (low >>> 1) ^ -(low & 1)
-}
-
-function decode64high(low, high) {
-  return (high >>> 1) ^ -(low & 1)
 }
 
 module.exports = {
